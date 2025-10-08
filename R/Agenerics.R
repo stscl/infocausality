626ad6f35eeca3ac82008ddb5d7c0d4d92f62239
SpatLyu <lyu.geosocial@gmail.com>
2025-10-09 00:12:53 +0800

    use s4 generics

---
+++ R/Agenerics.R
@@ -0,0 +1,12 @@
+register_generic = \(name, def = NULL) {
+  if (!methods::isGeneric(name)){
+    if (is.null(def)) {
+      def = eval(bquote(function(data, ...) standardGeneric(.(name))))
+    }
+    methods::setGeneric(name, def)
+  }
+}
+
+for (gen in c("surd")) {
+  register_generic(gen)
+}
