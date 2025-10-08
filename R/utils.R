7a9cc8ff20bffe882e18143a8fd69f3f17e0686a
SpatLyu <lyu.geosocial@gmail.com>
2025-10-08 23:04:56 +0800

    configure r and py interaction

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+utils_source_python = \(oauth_func_path) {
+  module_name = gsub("\\.py$", "", basename(oauth_func_path))
+  module_path = dirname(oauth_func_path)
+  reticulate::import_from_path(module_name, path = module_path, convert = TRUE)
+}
+
+utils_run_surd = \(obs, bin = 5L, max.combs = NULL, cores = 1){
+  ic = utils_source_python(system.file("python", "InfoCausality.py",
+                                       package = "infocausality"))
+  ict = ic$InfoCausality(obs, nbins = as.integer(bin))
+  ict$surd(max.combs,cores)
+}
