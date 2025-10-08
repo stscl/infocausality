5bf30a4f7d32e6b735afc38b92a8b986ba8b2d95
SpatLyu <lyu.geosocial@gmail.com>
2025-10-09 00:33:55 +0800

    update surd genercis

--- R/surd.R
+++ R/surd.R
@@ -1,21 +1,27 @@
 .surd_ts = \(data, target, agents, lag = 1, bin = 5, max.combs = NULL, cores = 1){
-  obs = cbind(data[,target,drop = TRUE],
-              RcppGenTSLagMulti(as.matrix(data[,agents,drop = FALSE])),lag)
+  obs = cbind(
+    data[,target,drop = TRUE],
+    RcppGenTSLagMulti(as.matrix(data[,agents,drop = FALSE]),lag)
+    )
   utils_run_surd(obs, bin, max.combs, cores)
 }
 
 .surd_lattice = \(data, target, agents, lag = 1, bin = 5, max.combs = NULL, cores = 1, nb = NULL){
   if (is.null(nb)) nb = sdsfun::spdep_nb(data)
   data = sf::st_drop_geometry(data)
-  obs = cbind(data[,target,drop = TRUE],
-              RcppGenLatticeLagMulti(as.matrix(data[,agents,drop = FALSE])),nb,lag)
+  obs = cbind(
+    data[,target,drop = TRUE],
+    RcppGenLatticeLagMulti(as.matrix(data[,agents,drop = FALSE]),nb,lag)
+    )
   utils_run_surd(obs, bin, max.combs, cores)
 }
 
 .surd_grid = \(data, target, agents, lag = 1, bin = 5, max.combs = NULL, cores = 1){
-  obs = cbind(terra::values(data[[target]],mat = TRUE,na.rm = FALSE),
-              RcppGenGridLagMulti(terra::values(data[[agents]],mat = TRUE,na.rm = FALSE)),
-                                  terra::nrow(data), lag)
+  obs = cbind(
+    terra::values(data[[target]],mat = TRUE,na.rm = FALSE),
+    RcppGenGridLagMulti(terra::values(data[[agents]],mat = TRUE,na.rm = FALSE),
+                        terra::nrow(data), lag)
+    )
   utils_run_surd(obs, bin, max.combs, cores)
 }
 
