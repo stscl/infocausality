9fdebe632467934d513abdc363b29c16db1661db
SpatLyu <lyu.geosocial@gmail.com>
2025-10-12 20:01:22 +0800

    update rcpp wrappers

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -10,6 +10,17 @@ Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
 Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
+// RcppDiscMat2PFM
+Rcpp::NumericVector RcppDiscMat2PFM(const Rcpp::NumericMatrix& x, double eps);
+RcppExport SEXP _infocausality_RcppDiscMat2PFM(SEXP xSEXP, SEXP epsSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::traits::input_parameter< const Rcpp::NumericMatrix& >::type x(xSEXP);
+    Rcpp::traits::input_parameter< double >::type eps(epsSEXP);
+    rcpp_result_gen = Rcpp::wrap(RcppDiscMat2PFM(x, eps));
+    return rcpp_result_gen;
+END_RCPP
+}
 // RcppGenLatticeLagUni
 Rcpp::NumericVector RcppGenLatticeLagUni(const Rcpp::NumericVector& vec, const Rcpp::List& nb, int lagNum);
 RcppExport SEXP _infocausality_RcppGenLatticeLagUni(SEXP vecSEXP, SEXP nbSEXP, SEXP lagNumSEXP) {
@@ -79,26 +90,15 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// RcppDiscMat2PFM
-Rcpp::NumericVector RcppDiscMat2PFM(const Rcpp::NumericMatrix& x, double eps);
-RcppExport SEXP _infocausality_RcppDiscMat2PFM(SEXP xSEXP, SEXP epsSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::traits::input_parameter< const Rcpp::NumericMatrix& >::type x(xSEXP);
-    Rcpp::traits::input_parameter< double >::type eps(epsSEXP);
-    rcpp_result_gen = Rcpp::wrap(RcppDiscMat2PFM(x, eps));
-    return rcpp_result_gen;
-END_RCPP
-}
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_infocausality_RcppDiscMat2PFM", (DL_FUNC) &_infocausality_RcppDiscMat2PFM, 2},
     {"_infocausality_RcppGenLatticeLagUni", (DL_FUNC) &_infocausality_RcppGenLatticeLagUni, 3},
     {"_infocausality_RcppGenLatticeLagMulti", (DL_FUNC) &_infocausality_RcppGenLatticeLagMulti, 3},
     {"_infocausality_RcppGenGridLagUni", (DL_FUNC) &_infocausality_RcppGenGridLagUni, 2},
     {"_infocausality_RcppGenGridLagMulti", (DL_FUNC) &_infocausality_RcppGenGridLagMulti, 3},
     {"_infocausality_RcppGenTSLagMultiSingle", (DL_FUNC) &_infocausality_RcppGenTSLagMultiSingle, 2},
     {"_infocausality_RcppGenTSLagMulti", (DL_FUNC) &_infocausality_RcppGenTSLagMulti, 2},
-    {"_infocausality_RcppDiscMat2PFM", (DL_FUNC) &_infocausality_RcppDiscMat2PFM, 2},
     {NULL, NULL, 0}
 };
 
