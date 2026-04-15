deb0f8c1e277213653432749648137eebe4cb22a
SpatLyu <lyu.geosocial@gmail.com>
2026-04-16 00:00:05 +0800

    add supersession noice in pkg startup message

--- R/zzz.R
+++ R/zzz.R
@@ -3,3 +3,8 @@
   loadNamespace("terra")
   reticulate::py_require(c("numpy","joblib"))
 }
+
+
+.onAttach <- function(...) {
+  packageStartupMessage(" Note: infocausality is superseded by the 'infoxtr' package.")
+}
